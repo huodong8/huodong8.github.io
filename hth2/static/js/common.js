3fdba968888f4abd610589324402e829b8e37e24
damao8 <damao1788@gmail.com>
2021-10-04 15:25:54 +0800

    202110041525

---
+++ hth2/static/js/common.js
@@ -0,0 +1,19 @@
+
+// PC注册链接
+function register() {
+
+    window.location.href = "https://www.hthbet619.com:7443/register/?i_code=7381993";
+}
+
+
+// mobi注册链接
+function registerH5() {
+    window.location.href = "https://www.hthty614.com:7443/entry/register/?i_code=7381993";
+}
+
+
+// downloadAPP链接
+function download() {
+    window.location.href = "https://www.htvip48.app:9960/?i_code=7381993";
+}
+
