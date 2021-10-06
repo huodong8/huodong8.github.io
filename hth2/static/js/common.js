a0629cec6264197b89ed74b13063613a529f0f3b
damao8 <damao1788@gmail.com>
2021-10-06 19:32:33 +0800

    a

--- hth2/static/js/common.js
+++ hth2/static/js/common.js
@@ -2,13 +2,13 @@
 // PC注册链接
 function register() {
 
-    window.location.href = "https://www.hthbet619.com:7443/register/?i_code=7381993";
+    window.location.href = "https://www.hthty616.com:7988/entry/register/?i_code=7381993";
 }
 
 
 // mobi注册链接
 function registerH5() {
-    window.location.href = "https://www.hthty614.com:7443/entry/register/?i_code=7381993";
+    window.location.href = "https://www.hthty616.com:7988/entry/register/?i_code=7381993";
 }
 
 
