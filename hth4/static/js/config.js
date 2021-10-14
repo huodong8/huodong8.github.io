40e8293090f5c573acb65bf7165a38eb5733746f
damao8 <damao1788@gmail.com>
2021-10-14 14:19:17 +0800

    1014

---
+++ hth4/static/js/config.js
@@ -0,0 +1,30 @@
+var config =  {
+  title: "", //站点名称
+
+  regLink_h5: "https://www.hthty615.com:7988/entry/register/?i_code=3765045", //手机h5端 注册链接
+  regLink_pc: "https://www.htvip48.app:9960/?i_code=3765045", //pc端 注册链接
+  
+
+  qq:[
+    "261819740",
+    //"810761444",
+  ],//qq号码
+
+  kefuLink_h5: "", //客服链接h5
+  kefuLink_pc: "", //客服链接pc
+
+   //表格
+   table:[
+     [100,28],
+     [200,38],
+     [500,88],
+     [5000,588],
+     [10000,888],
+     [20000,1288],
+  ],
+  
+  //表格-提款要求
+  require:"不限制游戏类型，1倍流水",
+
+  countJs: "" //统计代码链接
+};
