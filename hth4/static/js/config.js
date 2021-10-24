f3a958aceefddfc1a20dd7810c3db76ba5f6d114
damao8 <damao1788@gmail.com>
2021-10-24 10:49:03 +0800

    1024

--- hth4/static/js/config.js
+++ hth4/static/js/config.js
@@ -1,7 +1,7 @@
 var config =  {
   title: "", //站点名称
 
-  regLink_h5: "https://www.htvip23.app:2053/entry/register/?i_code=3765045", //手机h5端 注册链接
+  regLink_h5: "https://www.htvip22.app:20182/entry/register/?i_code=3765045", //手机h5端 注册链接
   regLink_pc: "https://www.htvip48.app:9960/?i_code=3765045", //pc端 注册链接
   
 
