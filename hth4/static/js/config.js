bc112d70a946a648ce65a40d99e0a26fd6e7ac49
damao8 <damao1788@gmail.com>
2021-11-13 19:14:48 +0800

    Update config.js

--- hth4/static/js/config.js
+++ hth4/static/js/config.js
@@ -1,8 +1,8 @@
 var config =  {
   title: "", //站点名称
    //9
-  regLink_h5: "https://www.9oqyz0u.com/entry/register/?i_code=6301433", //手机h5端 注册链接
-  regLink_pc: "https://www.9oqyz0u.com/?i_code=6301433", //pc端 注册链接
+  regLink_h5: "https://www.mt2kjg6.com/entry/register/?i_code=6301433", //手机h5端 注册链接
+  regLink_pc: "https://www.mt2kjg6.com/?i_code=6301433", //pc端 注册链接
   
 
   qq:[
