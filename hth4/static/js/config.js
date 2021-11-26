41476346df70e24560248635a165819dd2792fce
damao8 <damao1788@gmail.com>
2021-11-26 20:59:53 +0800

    Update config.js

--- hth4/static/js/config.js
+++ hth4/static/js/config.js
@@ -1,8 +1,8 @@
 var config =  {
   title: "", //站点名称
    //9
-  regLink_h5: "https://www.mvrglf4.com/entry/register/?i_code=6301433", //手机h5端 注册链接
-  regLink_pc: "https://www.mvrglf4.com/?i_code=6301433", //pc端 注册链接
+  regLink_h5: "https://www.09jsufg.com:6003/entry/register/?i_code=6301433", //手机h5端 注册链接
+  regLink_pc: "https://www.09jsufg.com:6003/?i_code=6301433", //pc端 注册链接
   
 
   qq:[
